33287e68e84ccf0161869b5b805e82c83f5d0f58
Lynn Leichtle <brooke.leichtle@gmail.com>
2024-06-24 00:08:29 +0200

    org updates

--- custom.el
+++ custom.el
@@ -7,7 +7,9 @@
    '("d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36"
      "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633"
      default))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(all-the-icons counsel doom-modeline ivy-rich lsp-ui meow org-bullets
+		   rust-mode solarized-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
