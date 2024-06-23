66d7db71f9d589abc253cb0b3d7f015a80e8a424
Lynn Leichtle <brooke.leichtle@gmail.com>
2024-06-23 19:58:04 +0200

    fixing for linux native

--- custom.el
+++ custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(rustic lsp-ui lsp-mode solarized-theme org-bullets counsel ivy-rich which-key avy dracula-theme doom-modeline swiper ivy)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
