3aae9b603d2cbaca2f19ddd7f40817eaa638dd29
Lynn Leichtle <brooke.leichtle@gmail.com>
2024-06-23 19:10:41 +0200

    rust maybe?

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(solarized-theme org-bullets counsel ivy-rich which-key avy dracula-theme doom-modeline swiper ivy)))
+   '(rustic lsp-ui lsp-mode solarized-theme org-bullets counsel ivy-rich which-key avy dracula-theme doom-modeline swiper ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
